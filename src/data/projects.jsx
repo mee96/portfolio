c23cbeeb87690d987c23855e786dcfa621003b97
mee96 <carmenmc96@gmail.com>
2025-11-26 07:02:46 +0100

    projectes acabat

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -4,24 +4,24 @@ const projectesData = [
         titol: "Purr Skills",
         descripcio: "Generador de fitxes de personatge per a jocs de ROL, amb funcions dinàmiques de càlcul de punts. Projecte realitzat en equip, on vaig assumir el lideratge i disseny.",
         
-        linkDemo: "https://angelacamu.github.io/project-promo-A-module-2-team-1/", // Substitueix pel link real
-        linkGithub: "https://angelacamu.github.io/project-promo-A-module-2-team-1/" // Substitueix pel link real
+        linkDemo: "https://angelacamu.github.io/project-promo-A-module-2-team-1/", 
+        linkGithub: "https://angelacamu.github.io/project-promo-A-module-2-team-1/" 
     },
     {
         id: 2,
         titol: "Rick & Morty API Search",
         descripcio: "Aplicació personal per a la cerca i el filtratge de personatges de la sèrie, utilitzant una API pública. Demostra el maneig de dades asíncrones i la gestió de l'estat amb Hooks.",
         
-        linkDemo: "https://beta.adalab.es/modulo-3-evaluacion-final-mee96/", // Substitueix pel link real
-        linkGithub: "https://github.com/Adalab/modulo-3-evaluacion-final-mee96" // Substitueix pel link real
+        linkDemo: "https://beta.adalab.es/modulo-3-evaluacion-final-mee96/", 
+        linkGithub: "https://github.com/Adalab/modulo-3-evaluacion-final-mee96" 
     },
     {
         id: 3,
         titol: "API Anime (Backend)",
         descripcio: "Projecte de backend implementant una base de dades amb animes i els seus personatges. Demostra coneixement d'arquitectura MERN/MEVN (segons les tecnologies utilitzades) i gestió de CRUD.",
         
-        linkDemo: "#", // Substitueix pel link real
-        linkGithub: "https://github.com/Adalab/modulo-4-evaluacion-final-bpw-mee96" // Substitueix pel link real
+        linkDemo: "#",
+        linkGithub: "https://github.com/Adalab/modulo-4-evaluacion-final-bpw-mee96" 
     }
 ];
 
