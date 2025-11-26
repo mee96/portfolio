40eaeffb1d844358e6dfb0fc2a23361de7415af0
mee96 <carmenmc96@gmail.com>
2025-11-26 07:01:14 +0100

    array projectes

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,52 +1,30 @@
+
+
 import "../scss/layout/Proyectos.scss";
 import { Element } from 'react-scroll';
 
-function proyectos() {
-  return (
 
+import projectesData from '../data/projects'; 
+
+
+import ProjectCard from './ProjectCard'; 
+
+
+function Proyectos() { 
+  return (
     <Element name="proyectos">
-    <h1>Mis proyectos</h1>
-    <div> 
-      
-    <div className="container">
-  <div className="box">
-    <span></span>
-    <div className="content">
-      <h2>Purr Skills</h2>
-      <p>proyecto en equipo que lideré y diseñé. Es un
-      generador de fichas de personaje para juegos de ROL</p>
-      <div className="boton"><a href="#">link</a>
-      <a href="#">github</a></div>
-      
-    </div>
-  </div>
-  <div className="box">
-    <span></span>
-    <div className="content">
-      <h2>Rick & Morty </h2>
-      <p>Lproyecto personal para
-buscar personajes de la serie Rick y morty usando una api
-publica.</p>
-<div className="boton"><a href="#">link</a>
-<a href="#">github</a></div>
-    </div>
-  </div>
-  <div className="box">
-    <span></span>
-    <div className="content">
-      <h2>API Anime</h2>
-      <p>proyecto de backend, base de datos con animes y
-personajes de estos.
-</p>
-<div className="boton"><a href="#">link</a>
-<a href="#">github</a></div>
-    </div>
-  </div>
-</div>
-  
-    </div>
+      <h1>Mis proyectos</h1>
+      <div className="container"> 
+        
+        
+        {projectesData.map((projecte) => (
+
+            <ProjectCard key={projecte.id} projecte={projecte} />
+        ))}
+        
+      </div>
     </Element>
-  )
+  );
 }
 
-export default proyectos
+export default Proyectos;
---
+++ src/data/projects.jsx
@@ -0,0 +1,28 @@
+const projectesData = [
+    {
+        id: 1,
+        titol: "Purr Skills",
+        descripcio: "Generador de fitxes de personatge per a jocs de ROL, amb funcions dinàmiques de càlcul de punts. Projecte realitzat en equip, on vaig assumir el lideratge i disseny.",
+        
+        linkDemo: "https://angelacamu.github.io/project-promo-A-module-2-team-1/", // Substitueix pel link real
+        linkGithub: "https://angelacamu.github.io/project-promo-A-module-2-team-1/" // Substitueix pel link real
+    },
+    {
+        id: 2,
+        titol: "Rick & Morty API Search",
+        descripcio: "Aplicació personal per a la cerca i el filtratge de personatges de la sèrie, utilitzant una API pública. Demostra el maneig de dades asíncrones i la gestió de l'estat amb Hooks.",
+        
+        linkDemo: "https://beta.adalab.es/modulo-3-evaluacion-final-mee96/", // Substitueix pel link real
+        linkGithub: "https://github.com/Adalab/modulo-3-evaluacion-final-mee96" // Substitueix pel link real
+    },
+    {
+        id: 3,
+        titol: "API Anime (Backend)",
+        descripcio: "Projecte de backend implementant una base de dades amb animes i els seus personatges. Demostra coneixement d'arquitectura MERN/MEVN (segons les tecnologies utilitzades) i gestió de CRUD.",
+        
+        linkDemo: "#", // Substitueix pel link real
+        linkGithub: "https://github.com/Adalab/modulo-4-evaluacion-final-bpw-mee96" // Substitueix pel link real
+    }
+];
+
+export default projectesData;
